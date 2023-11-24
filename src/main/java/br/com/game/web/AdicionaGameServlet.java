16d07405b9934a9222e0fb0068fa8a08333047e2
Lorena Emmerick de Oliveira <lorenae@ciandt.com>
2023-11-23 21:52:06 -0300

    ajustes nos servlets

--- src/main/java/br/com/game/web/AdicionaGameServlet.java
+++ src/main/java/br/com/game/web/AdicionaGameServlet.java
@@ -1,40 +1,87 @@
 package br.com.game.web;
 
 import br.com.game.web.entity.Game;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-@WebServlet("adicionaGame")
-public class AdicionaGameServlet  extends HttpServlet {
+@WebServlet("/adicionaGame")
+public class AdicionaGameServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Pegar os parâmetros da tela
         String img = request.getParameter("img");
         String title = request.getParameter("title");
         String starStr = request.getParameter("star");
         String reviews = request.getParameter("reviews");
         String prevPriceStr = request.getParameter("prevPrice");
-        String newPriceStr =request.getParameter("newPrice");
+        String newPriceStr = request.getParameter("newPrice");
         String company = request.getParameter("company");
         String consoleType = request.getParameter("consoleType");
         String category = request.getParameter("category");
 
+        // Ajustar os formatos do Objeto
         final Integer star = Integer.valueOf(starStr);
         final Double prevPrice = Double.valueOf(prevPriceStr);
         final Double newPrice = Double.valueOf(newPriceStr);
 
         final Game game = new Game(img, title, star, reviews, prevPrice, newPrice, company, consoleType, category);
 
+        // Envia a requisição POST para o servidor externo
+        String apiUrl = "http://localhost:9090/games/saveOne";
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        // Configuração da conexão
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setDoOutput(true);
+
+        // Construção do corpo da requisição
+        String jsonInputString = "{"
+                + "\"img\":\"" + img + "\","
+                + "\"title\":\"" + title + "\","
+                + "\"star\":" + star + ","
+                + "\"reviews\":\"" + reviews + "\","
+                + "\"prevPrice\":" + prevPrice + ","
+                + "\"newPrice\":" + newPrice + ","
+                + "\"company\":\"" + company + "\","
+                + "\"consoleType\":\"" + consoleType + "\","
+                + "\"category\":\"" + category + "\""
+                + "}";
+
+        // Envio do corpo da requisição
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes("UTF-8");
+            os.write(input, 0, input.length);
+        }
+
+        // Leitura da resposta
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"))) {
+            StringBuilder responseStringBuilder = new StringBuilder();
+            String responseLine = null;
+
+            while ((responseLine = br.readLine()) != null) {
+                responseStringBuilder.append(responseLine.trim());
+            }
+            System.out.println("POST One Game Resposta: " + responseStringBuilder.toString());
+        }
+
         PrintWriter out = response.getWriter();
-        out.println("<html><body><p> Game adicionado com sucesso! </p><p> Dados do Game: "+ game.toString() + " </p></body></html>" );
+        out.println("<html><body><p> Game adicionado com sucesso! </p><p> Dados do Game: " + game.toString() + " </p></body></html>");
 
-        System.out.println(game.toString());
-
-        //TODO: fazer a connection com o games_java API
+        // Fecha a conexão
+        connection.disconnect();
     }
 }
--- src/main/java/br/com/game/web/BuscaGameServlet.java
+++ src/main/java/br/com/game/web/BuscaGameServlet.java
@@ -13,29 +13,44 @@ import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
 
-@WebServlet("buscaGame")
+@WebServlet("/buscaGame")
 public class BuscaGameServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String id = request.getParameter("id");
 
-            //GET do GAME Externo - Com aplicação rodando local
-        URL url = new URL("http://localhost:9090/games/find/" + id);
-        URLConnection connection = url.openConnection();
-        InputStream inputStream = connection.getInputStream();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+        URLConnection connection = null;
+        InputStream inputStream = null;
+        BufferedReader bufferedReader = null;
 
-        String item = "";
-        StringBuilder json = new StringBuilder();
+        try {
+            // Envia a requisição GET para o servidor externo
+            URL url = new URL("http://localhost:9090/games/find/" + id);
+            connection = url.openConnection();
+            inputStream = connection.getInputStream();
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
 
-        while ((item = bufferedReader.readLine()) != null) {
-            json.append(item);
+            String item;
+            StringBuilder json = new StringBuilder();
+
+            while ((item = bufferedReader.readLine()) != null) {
+                json.append(item);
+            }
+
+            System.out.println("GET One Game: " + json.toString());
+
+            PrintWriter out = response.getWriter();
+            out.println("<html><body><p> Buscou o game com sucesso! </p><p> Dados do Game na busca: " + json.toString() + " </p></body></html>");
+        } finally {
+            // Fechar as instâncias no bloco finally para garantir que elas sejam fechadas,
+            // mesmo que ocorra uma exceção
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
         }
-
-        System.out.println("GET Game: " + json.toString());
-
-        PrintWriter out = response.getWriter();
-        out.println("<html><body><p> Buscou o game com sucesso! </p><p> Dados do Game Busca: " + json.toString() + " </p></body></html>");
     }
 }
